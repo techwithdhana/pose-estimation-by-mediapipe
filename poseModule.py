2de6a085dff62b3766c32a031571c0a514806c76
Dhanasekar M <77889053+techydhana@users.noreply.github.com>
2021-04-16 09:15:11 +0530

    Update poseModule.py

--- poseModule.py
+++ poseModule.py
@@ -48,12 +48,13 @@ def main():
         img = detector.findPose(img)
         lmList = detector.findPosition(img, draw=False)
         if len(lmList) != 0:
-            print(lmList[14])
-            cv2.circle(img, (lmList[14][1], lmList[14][2]), 15, (0, 0, 255), cv2.FILLED)
+            
+            cv2.circle(img, (lmList[0][1], lmList[0][2]), 15, (0, 0, 255), cv2.FILLED)
  
 
         cv2.imshow("Image", img)
-        cv2.waitKey(1)
+        if cv2.waitKey(1) & 0xFF == ord('s'):
+              break
  
  
 if __name__ == "__main__":
